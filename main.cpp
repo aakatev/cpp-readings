b2e5101b6b2b045d226ceb71f8d919505550eaea
aakatev <aakatev@mail.sfsu.edu>
2020-03-29 20:39:26 -0700

    Error handling added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <fstream>
 
+void error(std::string e);
+
 struct Reading {
   int hour;
   double temperature;
@@ -12,12 +14,13 @@ struct Reading {
 int main() {
   constexpr char file_name[] = "temperature.txt";
 
+  std::ifstream file(file_name, std::ios::in);
+  if (!file) error("Error openning file " + std::string(file_name) + "\n");
+  
   std::vector<Reading> readings;
   int hour;
   double temperature;
   std::string period;
-
-  std::ifstream file(file_name, std::ios::in);
 
   while (file >> hour >> temperature >> period) {
     readings.push_back(Reading{hour,temperature, period});
@@ -30,3 +33,6 @@ int main() {
   return 0;
 }
 
+void error(std::string e) {
+ throw std::runtime_error(e);
+}
